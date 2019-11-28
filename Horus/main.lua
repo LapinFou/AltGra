c67e89353ace36e76c70554ab714bb3042915438
LapinFou <lapin.iphone@gmail.com>
2019-11-28 18:34:47 +0100

    Ca commence à fonctionner. :)

--- Horus/main.lua
+++ Horus/main.lua
@@ -17,30 +17,8 @@ local defaultOptions = {
 local function creationWidget(zone, options)
   local maZone  = { zone=zone, options=options, parameters={} }
   
-  -- ID capteur d'altitude
---  maZone.parameters.alt_id = getFieldInfo(maZone.options.Capteur).id
---  maZone.parameters.altMax_id = getFieldInfo(maZone.options.Capteur.."+").id
-   if (maZone.options.Capteur == 0) then
-    if (getFieldInfo("Alt") ~= nil) then
-      maZone.options.Capteur = getFieldInfo("Alt").id
---      options.Capteur = getFieldInfo("Alt").id
-    else
-      maZone.options.Capteur = 240
-    end
-  end
- 
-  print("DEBUG: maZone.options.Capteur="..maZone.options.Capteur)
- 
   -- Démarre l'enregistrement si l'altitude est supérieure (valeur en mètre)
   maZone.parameters.altStart = 3
-  
-  -- Si "Auto" alors 'altStart' est utilisé, sinon choisir un inter 3 positions (par ex: "sb")
-  if (options.Auto == 0) then
-    maZone.parameters.altStartMode = maZone.options.Auto
-    maZone.parameters.inter_id = getFieldInfo(maZone.parameters.altStartMode).id
-  else
-    maZone.parameters.altStartMode = "Auto"
-  end
   
   -- Il faut choisir "true" ou "false": true va afficher le graphique en plein
   if (options.Plein == 0) then
@@ -50,33 +28,44 @@ local function creationWidget(zone, options)
   end
   
   -- Défini la taille d'affichage du graphique pour l'altitude: coordonnée en haut à gauche = (0,0)
-  maZone.parameters.originTps = 18     -- Origine de l'axe représentant le temps en seconde
-  maZone.parameters.largeurTps = 160   -- Largeur de l'axe représentant le temps en seconde
-  maZone.parameters.originAlt  = 62    -- Max de l'axe représentant l'altitude en mètre (l'axe Y est inversé !!)
-  maZone.parameters.hauteurAlt = 62    -- Largeur de l'axe représentant l'altitude en mètre
+  maZone.parameters.originTps = maZone.zone.x + 18     -- Origine de l'axe représentant le temps en seconde
+  
+  -- Largeur de l'axe représentant le temps en seconde
+  if (maZone.zone.w  > 380) then
+    maZone.parameters.largeurTps = maZone.zone.w - 52
+  else
+    maZone.parameters.largeurTps = maZone.zone.w - 18
+  end
+  
+  -- Origine de l'axe représentant l'altitude en mètre
+  maZone.parameters.originAlt  = maZone.zone.y
+  
+  -- Largeur de l'axe représentant l'altitude en mètre
+  maZone.parameters.hauteurAlt = maZone.zone.h
   
   -- Variables globales
   maZone.parameters.nbrLigneAlt = 6   -- Nombre de lignes horizontales
-  maZone.parameters.nbrPixelGrad = 0  -- Nombre de pixels par graduation
   maZone.parameters.newAlt = 0        -- Nouvelle altitude provenant du capteur
   maZone.parameters.maxAltAffi = 20   -- Altitude max affichable
   maZone.parameters.altMax = 0        -- Altitude max reçue par le vario
   maZone.parameters.tableAlt = {}     -- Tableau où sont stockes toutes les altitudes
   maZone.parameters.tableIndex = 0    -- Index indiquant jusqu'où est rempli le tableau
   maZone.parameters.gradAlt = 5       -- Altitude pour 1 graduation
-  maZone.parameters.pixelParMetre = 0 -- Valeur en mètre d'un pixel sur l'axe Y
-  maZone.parameters.secParPix = 0     -- Nombre de seconde par pixel sur l'axe X
   maZone.parameters.tempsMax = 20     -- Init du temps max en seconde
   maZone.parameters.tpsPrec = 0       -- Temps de la dernière mise à jour du tableau
   maZone.parameters.compTemps = 6     -- Compression du temps compTemps/(compTemps-1) - La valeur mini est 2
   maZone.parameters.grdeAlt = false   -- Décalage du graphique vers la droite si l'altitude est supérieure à 960m
   maZone.parameters.startAlt = false  -- Démarre l'enregistrement
 
+  -- Nombre de seconde par pixel sur l'axe X
   -- Init le nombre de seconde par pixel sur l'axe X
   maZone.parameters.secParPix = 100*maZone.parameters.tempsMax/maZone.parameters.largeurTps
   
+  -- Nombre de pixels par graduation
   -- Calcul nombre de pixel pour 1 graduation (arrondi inférieur)
   maZone.parameters.nbrPixelGrad = math.floor(maZone.parameters.hauteurAlt/maZone.parameters.nbrLigneAlt)
+  
+  -- Valeur en mètre d'un pixel sur l'axe Y
   -- Mettre à jour l'échelle de l'axe altitude
   maZone.parameters.pixelParMetre = maZone.parameters.nbrPixelGrad/maZone.parameters.gradAlt
 
@@ -113,6 +102,7 @@ local function majWidget(widgetToUpdate, newOptions)
   
   --  print(widgetToUpdate)
   widgetToUpdate.options = newOptions
+  widgetToUpdate.options.Inter = 0
 
   if (newOptions.Capteur == 0) then
     if (getFieldInfo("Alt") ~= nil) then
@@ -141,10 +131,53 @@ local function majWidget(widgetToUpdate, newOptions)
 end
 
 
+local function dessinerGrille(originTps, originAlt, largeurTps, hauteurAlt, nbrLigneAlt, nbrPixelGrad)
+  -- Utiliser pour marquer la graduation à droite des chiffres (1 pixel)
+  local pointAlt
+
+  lcd.drawFilledRectangle(originTps, originAlt, largeurTps, hauteurAlt, SOLID + WHITE)
+  lcd.drawRectangle(originTps-1, originAlt-1, largeurTps+2, hauteurAlt+2, SOLID, 2)
+  
+  -- Dessine les lignes horizontales
+  for index = 1, nbrLigneAlt do
+    pointAlt = originAlt+hauteurAlt-nbrPixelGrad*index+1
+    lcd.drawLine(originTps, pointAlt, originTps+largeurTps-1, pointAlt, DOTTED, GREY)
+    lcd.drawPoint(originTps-2, pointAlt)
+  end
+
+--  return
+end
+
 local function tacheDeFondWidget(widgetToProcessInBackground)
+  return widgetToProcessInBackground
 end
 
 local function rafraichitWidget(widgetToRefresh)
+--  dessinerAxe({ originTps=widgetToRefresh.parameters.originTps,
+--                originAlt=widgetToRefresh.parameters.originAlt,
+--                largeurTps=widgetToRefresh.parameters.largeurTps,
+--                hauteurAlt=widgetToRefresh.parameters.hauteurAlt})
+  dessinerGrille(widgetToRefresh.parameters.originTps,
+                 widgetToRefresh.parameters.originAlt,
+                 widgetToRefresh.parameters.largeurTps,
+                 widgetToRefresh.parameters.hauteurAlt,
+                 widgetToRefresh.parameters.nbrLigneAlt,
+                 widgetToRefresh.parameters.nbrPixelGrad)
+  
+  
+--  lcd.drawFilledRectangle(widgetToRefresh.zone.x, widgetToRefresh.zone.y, 40, 40, SOLID + WHITE)
+--  lcd.drawRectangle(widgetToRefresh.zone.x, widgetToRefresh.zone.y, 40, 40, SOLID)
+  
+--  lcd.drawFilledRectangle(widgetToRefresh.parameters.originTps,
+--                          widgetToRefresh.parameters.originAlt,
+--                          widgetToRefresh.parameters.largeurTps,
+--                          widgetToRefresh.parameters.hauteurAlt,
+--                          SOLID + WHITE)
+--  lcd.drawRectangle(widgetToRefresh.parameters.originTps, widgetToRefresh.parameters.originAlt, 40, 40, SOLID)
+  
+  --lcd.drawRectangle(widgetToRefresh.zone.x, widgetToRefresh.zone.y, 40, 40, SOLID)
+
+  -- return widgetToRefresh
 end
 
 return { name="AltGra", options=defaultOptions, create=creationWidget, update=majWidget, background=tacheDeFondWidget, refresh=rafraichitWidget }
